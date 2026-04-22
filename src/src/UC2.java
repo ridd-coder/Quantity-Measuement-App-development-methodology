8ab490b090dcd99aef120cc33cd34769d4653cc8
Riddhiman Dutta <riddhimandutta@Riddhimans-MacBook-Air-9.local>
2026-04-22 14:35:52 +0530

    UC2: Add Feet & Inches equality

---
+++ src/src/UC2.java
@@ -0,0 +1,68 @@
+public class UC2 {
+
+    static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+
+            Feet other = (Feet) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Double.hashCode(value);
+        }
+    }
+
+    static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+
+            Inches other = (Inches) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Double.hashCode(value);
+        }
+    }
+
+    public static boolean compareFeet(double v1, double v2) {
+        Feet f1 = new Feet(v1);
+        Feet f2 = new Feet(v2);
+        return f1.equals(f2);
+    }
+
+    public static boolean compareInches(double v1, double v2) {
+        Inches i1 = new Inches(v1);
+        Inches i2 = new Inches(v2);
+        return i1.equals(i2);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Comparing 1.0 ft and 1.0 ft: " + compareFeet(1.0, 1.0));
+
+        System.out.println("Comparing 1.0 inch and 1.0 inch: " + compareInches(1.0, 1.0));
+
+        System.out.println("Comparing 1.0 inch and 2.0 inch: " + compareInches(1.0, 2.0));
+    }
+}
+
